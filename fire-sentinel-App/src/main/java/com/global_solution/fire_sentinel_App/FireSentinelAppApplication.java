09530159b346d1ad280ed29a9cf6678edf048354
Rafael <rafaelgasparmartins@icloud.com>
2025-05-26 21:07:45 -0300

    first commit

---
+++ fire-sentinel-App/src/main/java/com/global_solution/fire_sentinel_App/FireSentinelAppApplication.java
@@ -0,0 +1,13 @@
+package com.global_solution.fire_sentinel_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireSentinelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireSentinelAppApplication.class, args);
+	}
+
+}
---
+++ fire-sentinel-App/src/test/java/com/global_solution/fire_sentinel_App/FireSentinelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.global_solution.fire_sentinel_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireSentinelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
